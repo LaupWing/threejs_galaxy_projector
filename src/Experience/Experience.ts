e26164ca908370af5930a7ff2e8bfe8fdb007a8e
LaupWing <loc_strict@hotmail.com>
2023-01-09 14:57:38 +0100

    udpated

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,7 +1,20 @@
+import Sizes from "./Utils/Sizes"
+import Time from "./Utils/Time"
+import * as THREE from "three"
+import Camera from "./Camera"
+
 export default class Experience {
    canvas: HTMLCanvasElement
+   sizes: Sizes
+   time: Time
+   scene: THREE.Scene
+   camera: Camera
 
    constructor(canvas:HTMLCanvasElement){
       this.canvas = canvas
+      this.sizes = new Sizes()
+      this.time = new Time()
+      this.scene = new THREE.Scene()
+      this.camera = new Camera(this)
    }
 }
