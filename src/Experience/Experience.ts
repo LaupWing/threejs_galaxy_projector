8c8f6ff7483087fe1f12ad09a75f3b6ea2cec7f3
LaupWing <loc_strict@hotmail.com>
2023-01-10 14:57:14 +0100

    initialize

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -4,6 +4,7 @@ import * as THREE from "three"
 import Camera from "./Camera"
 import Renderer from "./Renderer"
 import World from "./World/World"
+import Debug from "./Utils/Debug"
 
 export default class Experience {
    canvas: HTMLCanvasElement
@@ -12,10 +13,12 @@ export default class Experience {
    scene: THREE.Scene
    camera: Camera
    renderer: Renderer
+   debug: Debug
    world: World
 
    constructor(canvas:HTMLCanvasElement){
       this.canvas = canvas
+      this.debug = new Debug()
       this.sizes = new Sizes()
       this.time = new Time()
       this.scene = new THREE.Scene()
--- src/Experience/World/GenerateGalaxy/GenerateGalaxy.ts
+++ src/Experience/World/GenerateGalaxy/GenerateGalaxy.ts
@@ -1,16 +1,29 @@
 import * as THREE from "three"
 import Experience from "../../Experience"
+import Debug from "../../Utils/Debug"
 
 export default class GenerateGalaxy {
+   amount: number
    geometry: THREE.BufferGeometry
    scene: THREE.Scene
+   debug: Debug
 
    constructor(amount:number, experience: Experience){
+      this.amount = amount
       this.geometry = new THREE.BufferGeometry()
       this.scene = experience.scene
-      const positions = new Float32Array(amount * 3)
+      this.debug = experience.debug
+      this.setDebug()
+   }
+
+   setDebug(){
+
+   }
+
+   initialize(){
+      const positions = new Float32Array(this.amount * 3)
       
-      const array = [...Array(amount)]
+      const array = [...Array(this.amount)]
       array.forEach((_, i:number) => {
          const i3 = i * 3
 
